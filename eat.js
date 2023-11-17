f6a3e2207f312f465a65d3dd0bb93ad156139142
Sadie James <samajeidas@sadies-air.mynetworksettings.com>
2023-11-17 15:18:04 -0500

    All pages work, without CSS

--- eat.js
+++ eat.js
@@ -1,11 +1,20 @@
 const get_meal_btn = document.getElementById('get_meal');
 const meal_container = document.getElementById('meal');
 
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(res => res.json())
+.then(res => {
+createMeal(res.meals[0]);
+// console.log(res.meals[0]);
+});
+
+
 get_meal_btn.addEventListener('click', () => {
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(res => {
     createMeal(res.meals[0]);
+    // console.log(res.meals[0]);
   });
 });
 
